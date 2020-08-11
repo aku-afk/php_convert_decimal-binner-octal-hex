6b52ba7f91b3bb3d43fcfc3960c545cceab09df0
aku-afk <64795908+aku-afk@users.noreply.github.com>
2020-08-11 23:32:01 +0700

    Add files via upload

---
+++ conf/05.php
@@ -0,0 +1,11 @@
+<?
+    print("Enter Binner Number : \n");
+    print("\033[0;32m#  \033[1;37m");
+    $num_des = trim(fgets(STDIN));
+    print("Octal Number \n");
+    $res = decoct(bindec($num_des));
+    print("\033[0;32m#  \033[0;36m".$res."\033[1;34m\033[1;37m \n");
+print("\n");
+print("###################### \n");
+print("\n");
+include 'algo_r.php';
