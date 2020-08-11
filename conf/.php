c46c8e26441a5a4c56bf04d75a17f856b9806d01
aku-afk <64795908+aku-afk@users.noreply.github.com>
2020-08-11 16:06:34 +0700

    Add files via upload

---
+++ conf/.php
@@ -0,0 +1,8 @@
+<?
+    print("\033[1;31m#  Tidak Ada Perintah\n   Seperti Itu\033[1;37m\n\n");
+    print("  \033[1;31m[!]\033[1;37m PILIH YANG BENER\n  \033[1;31m[!]\033[1;37m ASTAGHFIRULLAH\n");
+print("\n");
+print("###################### \n");
+print("\n");
+include 'algo_r.php';
+print("\n");
