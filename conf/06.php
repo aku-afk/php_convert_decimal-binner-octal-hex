1a5788590d41bff660a62933280bd4546384d407
aku-afk <64795908+aku-afk@users.noreply.github.com>
2020-08-11 14:47:08 +0700

    Add files via upload

---
+++ conf/06.php
@@ -0,0 +1,12 @@
+<?
+    print("Enter Binner Number : \n");
+    print("\033[1;32m#  \033[1;37m");
+    $num_des = trim(fgets(STDIN));
+    print("Hex Number \n");
+    $res = dechex(bindec($num_des));
+    print("\033[1;32m#  \033[0;36m".$res."\033[1;34m\033[1;37m \n");
+print("\n");
+print("###################### \n");
+print("\n");
+include 'algo_r.php';
+print("\n");
---
+++ conf/99.php
@@ -0,0 +1,3 @@
+<?
+print("[::]   Terimakasih Udah \n[::]   Pake Toolsnya \n[::]   Tapi Tolong\n\n");
+print("[::]   \033[1;31mLAIN KALI BELAJAR \n\033[1;37m[::]   \033[1;31mASTAGHFIRULLAH\033[1;37m \n\n");
