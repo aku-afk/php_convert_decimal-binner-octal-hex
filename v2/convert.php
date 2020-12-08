52dd7f97448a09452afbaec295a0f8b81cbec4f4
aku-afk <64795908+aku-afk@users.noreply.github.com>
2020-12-08 22:08:11 +0700

    Create convert.php

---
+++ v2/convert.php
@@ -0,0 +1,17 @@
+<?
+/*
+AUTHORNYA JANGAN DIGANTI ASTAGHFIRULLAH
+kalian yang ganti nama authornya, tolong
+tanggung sendiri dosanya
+*/
+print("\n\n");
+print("###################### \n");
+print("                       \n");
+print("   TERMUX_CONVERTER   \n");
+print("                       \n");
+print("###################### \n");
+print("   author : RahmatK    \n");
+print("   kelas  : X_MM       \n");
+print("                       \n");
+print("###################### \n\n");
+include 'algo_r.php';
