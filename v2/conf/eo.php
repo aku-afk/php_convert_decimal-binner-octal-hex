a797192c69a4e783943233e8ad625e534ecda613
aku-afk <64795908+aku-afk@users.noreply.github.com>
2020-12-08 22:13:17 +0700

    Create eo.php

---
+++ v2/conf/eo.php
@@ -0,0 +1,7 @@
+<?
+    print("\033[1;31m#  Tidak Ada Perintah\n   Seperti Itu\033[1;37m\n\n");
+    print("  \033[1;31m[!]\033[1;37m PILIH YANG BENER\n  \033[1;31m[!]\033[1;37m ASTAGHFIRULLAH\n");
+print("\n");
+print("###################### \n");
+print("\n");
+include 'algo_r.php';
